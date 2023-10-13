2c35526eb43dd8abf9945933f1c3a55143e1dfbe
corinachistol <chistolcorina19@gmail.com>
2023-10-13 22:09:17 +0300

    click events & functions -lesson7

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+
+    const  handleclick = (e) => {
+        console.log('Hello ninjas',e)
+    }
+    const  handleclickAgain = (name,e) => {
+        console.log('hello' + name,e)
+    }
+
+
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick = { handleclick }>Click me</button>
+            <button onClick={ (e) => handleclickAgain('mario',e)}>Click me again</button>
         </div>
      );
 }
