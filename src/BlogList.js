b2b58f6263cf4e2a026c451d66d294ea7d2c6459
corinachistol <chistolcorina19@gmail.com>
2023-10-22 14:56:56 +0300

    fetching data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList  = ( {blogs,title, handleDelete} ) => {  
+const BlogList  = ( {blogs,title, } ) => {  
     // console.log(props) 
 
     return ( 
@@ -8,7 +8,6 @@ const BlogList  = ( {blogs,title, handleDelete} ) => {
             return <div className="blog-preview" key={blog.id}>
                 <h2>{ blog.title }</h2>
                 <p>Written by { blog.author }</p>
-                <button onClick={ () => handleDelete(blog.id)}>Delete Blog</button>
             </div>
            })} 
         </div>
--- src/Home.js
+++ src/Home.js
@@ -3,33 +3,26 @@ import BlogList from "./BlogList";
 
 const Home = () => {
                             // when the state changes, triggers a rerender -> useEffect ->triggers useState ->triggers a useEffect   .... it can be a continous loop
-    const [blogs, setBlog] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+    const [blogs, setBlog] = useState(null)
 
-    const [name, setName] = useState("mario")
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter( (blog) => blog.id !== id )
-        setBlog(newBlogs)
-    }
-
+   
     // every time there is a rerender ,the callback function will run.First at initial load, then every time the state(useState) changes. Useful for fetching data
     useEffect( () => {
-        console.log('use effect ran!')
-        console.log(name)
-    },[name] )
+       fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data)
+            setBlog(data)
+        });
+    },[] )
     // [] = empty dependancy arrays makes sure that this hook useEffect ran only once, at first render!
     // [name] = useEffect will ran at firs render and then everytime when the name changes
 
     return ( 
         <div className="home">
-                        {/* here is the object props defined in arguments of the child */}
-            <BlogList blogs ={blogs} title="All Blogs!" handleDelete={handleDelete} />  
-            <button onClick={ () => setName("luigi") }>Change name</button>
-            <p>{name}</p>
+           {blogs && <BlogList blogs ={blogs} title="All Blogs!" /> }
         </div>
      );
 }
